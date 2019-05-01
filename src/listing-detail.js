2ffb83b7ee8de4a4a6e1cef7a6bb55202afca55e
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-05-01 10:53:39 -0700

    finished the js for listing detail page and updated the links on all the pages to look a little better

--- src/listing-detail.js
+++ src/listing-detail.js
@@ -1,9 +1,23 @@
 import listingApi from './listing-api.js'
 
 const year = document.getElementById('year');
-const year = document.getElementById('year');
-const year = document.getElementById('year');
-const year = document.getElementById('year');
-const year = document.getElementById('year');
-const year = document.getElementById('year');
-const year = document.getElementById('year');
+const unknownYear = document.getElementById('unknown year');
+const continent = document.getElementById('continent');
+const goldOrNot = document.getElementById('yes-gold');
+const multiMaterial = document.getElementById('yes-multi');
+const composition = document.getElementById('composition');
+const condition = document.getElementById('condition');
+
+const listing = listingApi.get();
+
+if(!listing) {
+    window.location = './';
+}
+
+year.textContent = listing.year;
+unknownYear.textContent = listing.unknownYear;
+continent.textContent = listing.continent;
+goldOrNot.textContent = listing.goldOrNot;
+multiMaterial.textContent = listing.multiMaterial;
+composition.textContent = listing.composition;
+condition.textContent = listing.condition;
