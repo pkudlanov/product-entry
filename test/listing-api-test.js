cd9d9cd6e2f092f4d251531cc08d0c5d6756b9b6
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-05-03 11:57:29 -0700

    wrote test which I believes clears storage and checks that its cleared

--- test/listing-api-test.js
+++ test/listing-api-test.js
@@ -1,7 +1,12 @@
 import listingApi from '../src/listing-api.js';
 const test = QUnit.test;
 
+QUnit.module('listing api')
+listingApi.storage = sessionStorage;
+const testStorage = sessionStorage;
+
 test('round trip listing object', function(assert) {
+    testStorage.removeItem('listings');
     const listing = {
         continent: 'south america'
     };
@@ -10,4 +15,13 @@ test('round trip listing object', function(assert) {
     const result = listingApi.get();
 
     assert.deepEqual(result, listing);
+});
+
+test('removing listings in local storage, returns empty array', function(assert){
+    testStorage.removeItem('listings');
+    const expected = [];
+
+    const listings = listingApi.getAll();
+
+    assert.deepEqual(listings, expected);
 });
